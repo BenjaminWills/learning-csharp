c9fbceeb1c1a3abf73cf21a727d65a157d3348a6
BenjaminWills <benjaminwills047@gmail.com>
2022-10-25 19:51:38 +0100

    Initialised first application!

---
+++ first_console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string my_name = args[0];
+            Console.WriteLine("Hello World from " + my_name);
+        }
+    }
+}
