a563fb9f9e2004b30ce60f623af8a8704f7b3128
BenjaminWills <benjaminwills047@gmail.com>
2022-10-25 23:14:10 +0100

    Updated program

--- user_interactive_console/Program.cs
+++ user_interactive_console/Program.cs
@@ -4,8 +4,10 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the addition calculator");
             Console.WriteLine("Input an integer");
             var num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input an integer");
             var num2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"The sum of {num1} and {num2} is {num1+num2}");
         }
